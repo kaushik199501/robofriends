9da854cbbc74c5ea151def2003f934acd194f17c
Kaushik Koparkar <koparkar.kaushik@gmail.com>
2020-07-18 10:45:37 +0530

    Updates

---
+++ precache-manifest.e9002d8773b77c2a2b25deb0be2f61fd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e3a3fda65176ea155d033d1632cf1e1",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "005ad378bc2817b28083",
+    "url": "/robofriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "d2d8ee3038d0b20357e5",
+    "url": "/robofriends/static/css/main.5aed6d9c.chunk.css"
+  },
+  {
+    "revision": "005ad378bc2817b28083",
+    "url": "/robofriends/static/js/2.f690e5d6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robofriends/static/js/2.f690e5d6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d2d8ee3038d0b20357e5",
+    "url": "/robofriends/static/js/main.38b28b0d.chunk.js"
+  },
+  {
+    "revision": "e7d2cbb368e0813da61d",
+    "url": "/robofriends/static/js/runtime-main.74eee7b5.js"
+  }
+]);
